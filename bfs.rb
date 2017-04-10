7213ae4700c46307a26d6c7c0e88ff18abe3a055
Oleh Sliusar <olehsliusar@gmail.com>
2017-04-10 17:45:28 +0300

    Weight the graph

--- bfs.rb
+++ bfs.rb
@@ -12,6 +12,17 @@ def do_bfs(graph, source)
 
   queue = Queue.new
   queue.enqueue(source)
-  
+  while !queue.is_empty?
+    u = queue.dequeue
+
+    graph[u].each do |v|
+      if bfs_info[v][:distance].nil?
+        queue.enqueue(v)
+        bfs_info[v][:distance] = bfs_info[u][:distance] + 1
+        bfs_info[v][:predecessor] = u
+      end
+    end
+  end
+
   bfs_info
 end
