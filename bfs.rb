387e70920d199319400ec2568ac57800c9c9fd40
Oleh Sliusar <olehsliusar@gmail.com>
2017-04-10 17:10:43 +0300

    Initialize data for bfs_info

--- bfs.rb
+++ bfs.rb
@@ -1,4 +1,14 @@
 require_relative 'queue'
 def do_bfs(graph, source)
+  bfs_info = []
+  graph.length.times do |i|
+    bfs_info[i] = {
+      distance: nil,
+      predecessor: nil
+    }
+  end
 
+  bfs_info[source][:distance] = 0;
+
+  bfs_info
 end
